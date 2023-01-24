66e2c9f4f8f615e269919d40be14c86b6c733a2d
AshOConnor <s3915744@student.rmit.edu.au>
2023-01-24 14:44:52 +1100

    Add debugModule() & myCode() to bottom of pages.

--- a3/index.php
+++ a3/index.php
@@ -300,17 +300,7 @@ require 'tools.php';
       <button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
     </div>
   </footer>
-  <aside id="debug">
-    <hr>
-    <h3>Debug Area</h3>
-    <pre>
-GET Contains:
-<?php print_r($_GET) ?>
-POST Contains:
-<?php print_r($_POST) ?>
-SESSION Contains:
-<?php print_r($_SESSION) ?>
-      </pre>
-  </aside>
 </body>
 </html>
+<?= debugModule() ?>
+<?= myCode() ?>
