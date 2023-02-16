b9467a44337a084926b303e4aa50d65583a5a358
AshOConnor <s3915744@student.rmit.edu.au>
2023-02-16 15:14:09 +1100

    add movies javascript array back.

--- a3/script.js
+++ a3/script.js
@@ -1,4 +1,38 @@
 /* Insert your javascript here */
+const movies = [
+  {
+    id: 'ACT',
+    title: "Avatar: The Way of Water",
+    rating: "PG",
+    plot: "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+    trailer: "https://www.imdb.com/videoembed/vi3565864217",
+    bookingTimeList: ["Wed - 2100","Thu - 2100","Fri - 2100","Sat - 1800","Sun - 1800"]
+  },
+  {
+    id: 'RMC',
+    title: "Weird: The Al Yankovic Story",
+    rating: "TV",
+    plot: "Explores every facet of Yankovic's life, from his meteoric rise to fame with early hits like 'Eat It' and 'Like a Surgeon' to his torrid celebrity love affairs and famously depraved lifestyle.",
+    trailer: "https://www.imdb.com/videoembed/vi1080541721",
+    bookingTimeList: ["Wed - 1200","Thu - 1200","Fri - 1200","Sat - 1500","Sat - 1500"]
+  },
+  {
+    id: 'ANM',
+    title: "Puss in Boots: The Last Wish",
+    rating: "PG",
+    plot: "When Puss in Boots discovers that his passion for adventure has taken its toll and he has burned through eight of his nine lives, he launches an epic journey to restore them by finding the mythical Last Wish.",
+    trailer: "https://www.imdb.com/videoembed/vi3470049817",
+    bookingTimeList: ["Mon - 1200","Tue - 1200","Wed - 1800","Thu - 1800","Fri - 1800","Sat - 1200","Sat - 1200"]
+  },
+  {
+    id: 'AHF',
+    title: "Margrete: Queen of the North",
+    rating: "MA",
+    plot: "1402. Queen Margrete is ruling Sweden, Norway and Denmark through her adopted son, Erik. But a conspiracy is in the making and Margrete finds herself in an impossible dilemma that could shatter her life's work: the Kalmar Union.",
+    trailer: "https://https://www.imdb.com/videoembed/vi968475417",
+    bookingTimeList: ["Won - 1200","Tue - 1200","Sat - 2100","Sat - 2100"]
+  },
+];
 
 function generateTickNo() {
   var seatOptions = "<option value=''>Please Select</option>";
